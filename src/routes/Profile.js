bdea788cd85527c90c67804fd736b07ef0e6b9f6
hanum <rntls123@naver.com>
2021-09-22 23:09:01 +0900

    09.22 Render own chats

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,16 +1,30 @@
 import { db, dbService } from "firebaseSetting";
 import React, { useEffect } from "react";
+import { useState } from "react/cjs/react.development";
+import Chat from "components/Chat";
 
-export default ({ userObj }) => {
+const Profile = ({ userObj }) => {
+    const [chats, setChats] = useState([]);
     const getMyChats = async () => {
         const q = dbService.query(dbService.collection(db, "chats"), dbService.where("creatorId", "==", userObj.uid), dbService.orderBy("createdAt"));
-        const chats = await dbService.getDocs(q);
-        console.log(chats.docs.map((doc) => doc.data()));
+        const docs = await dbService.getDocs(q);
+        const chats = docs.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+        setChats(chats);
     };
     useEffect(() => {
         getMyChats();
     }, []);
     return (
-        <span>Profile</span>
+        <>
+            <span>Profile</span>
+            <div>
+                {chats.map((chat) => (
+                    <Chat key={chat.id} chatObj={chat} isOwner={chat.creatorId === userObj.uid} />
+                )
+                )}
+            </div>
+        </>
     )
 };
+
+export default Profile;
